7310c3e37b5eba685a7a8efd99f318322030c7f8
bvpraveen <ursoftpraveen@gmail.com>
2013-12-16 23:34:19 +0530

    Revert "Adding project"
    This reverts commit f251efb6f67c6883aa0e3320b083f88314809d77.

--- iRent/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$dbc= mysqli_connect('localhost:3306','root','Password~1','irent') or die('Error connect to MYSQL server');
-mysql_select_db("irent") or die ("Could not Find the database irent.");
-
-?>
